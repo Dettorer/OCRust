67701bbcf2639a8d369901934b24e233a3bdd69d
Paul Dettorer Hervot <dettorer@dettorer.net>
2020-05-20 00:58:13 +0200

    mlp: rename bench tests for consistency with other tests

--- tests/mlp-benchmarking.rs
+++ tests/mlp-benchmarking.rs
@@ -8,19 +8,19 @@ use nalgebra::DVector;
 use test::Bencher;
 
 #[bench]
-fn bench_classify_mlp(bencher: &mut Bencher) {
+fn bench_mlp_classify(bencher: &mut Bencher) {
     let network = mlp![20, 15, 15, 10];
     let input = DVector::repeat(20, 10.);
     bencher.iter(|| network.classify(&input));
 }
 
 #[bench]
-fn bench_randomize_mlp(bencher: &mut Bencher) {
+fn bench_mlp_randomize(bencher: &mut Bencher) {
     bencher.iter(|| randomized_mlp![20, 15, 15, 13, 17, 10]);
 }
 
 #[bench]
-fn bench_learning_mlp(bencher: &mut Bencher) {
+fn bench_mlp_learning(bencher: &mut Bencher) {
     let mut network = mlp![20, 15, 15, 10];
     let input = DVector::repeat(20, 10.);
     bencher.iter(|| network.train_case(&input, 5));
