d4ab2ee501c3f89d5b19e1b15add2bceed6902f3
Paul Dettorer Hervot <dettorer@dettorer.net>
2020-05-20 00:57:29 +0200

    mlp: use a bigger network for the learning benchmark

--- tests/mlp-benchmarking.rs
+++ tests/mlp-benchmarking.rs
@@ -21,9 +21,7 @@ fn bench_randomize_mlp(bencher: &mut Bencher) {
 
 #[bench]
 fn bench_learning_mlp(bencher: &mut Bencher) {
-    let mut network = mlp![2;2];
-    bencher.iter(|| {
-        let input = DVector::from_row_slice(&[0_f64, 1_f64]);
-        network.train_case(&input, 1)
-    });
+    let mut network = mlp![20, 15, 15, 10];
+    let input = DVector::repeat(20, 10.);
+    bencher.iter(|| network.train_case(&input, 5));
 }
